e5210c21fb112ff81aa973fdac977a68ea32b62c
pedrohlsa <stguts25@gmail.com>
2026-06-21 22:36:31 -0300

    Add sizeof_types.c to display sizes of data types

---
+++ Sandbox/sizeof_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    char j;
+    printf("Tamanho de int: %zu bytes\n", sizeof(x));
+    printf("Tamanho de char: %zu bytes\n", sizeof(j));
+
+    return 0;
+}
